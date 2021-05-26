d37e771bb5018fc685bfabaf678719062054d5d5
Mariano Alvarez <marianoa@gmail.com>
2021-05-26 10:33:57 -0400

    Initial commit

--- src/components/ConfigScreen.tsx
+++ src/components/ConfigScreen.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { AppExtensionSDK } from '@contentful/app-sdk';
-import { Heading, Form, Workbench, Paragraph } from '@contentful/forma-36-react-components';
+import { Heading, List, ListItem, Switch, Form, Workbench, Paragraph } from '@contentful/forma-36-react-components';
 import { css } from 'emotion';
 
 export interface AppInstallationParameters {}
@@ -10,58 +10,142 @@ interface ConfigProps {
 }
 
 interface ConfigState {
-  parameters: AppInstallationParameters;
+  data: { active: boolean; contentType: any }[];
 }
 
 export default class Config extends Component<ConfigProps, ConfigState> {
   constructor(props: ConfigProps) {
     super(props);
-    this.state = { parameters: {} };
 
-    // `onConfigure` allows to configure a callback to be
-    // invoked when a user attempts to install the app or update
-    // its configuration.
+    this.state = {
+      data: [],
+    };
+
     props.sdk.app.onConfigure(() => this.onConfigure());
-  }
-
-  async componentDidMount() {
-    // Get current parameters of the app.
-    // If the app is not installed yet, `parameters` will be `null`.
-    const parameters: AppInstallationParameters | null = await this.props.sdk.app.getParameters();
-
-    this.setState(parameters ? { parameters } : this.state, () => {
-      // Once preparation has finished, call `setReady` to hide
-      // the loading screen and present the app to a user.
-      this.props.sdk.app.setReady();
-    });
   }
 
   onConfigure = async () => {
     // This method will be called when a user clicks on "Install"
-    // or "Save" in the configuration screen.
-    // for more details see https://www.contentful.com/developers/docs/extensibility/ui-extensions/sdk-reference/#register-an-app-configuration-hook
+    // or "Save" on the configuration screen.
+    // For more details see https://www.contentful.com/developers/docs/extensibility/ui-extensions/sdk-reference/#register-an-app-configuration-hook
 
-    // Get current the state of EditorInterface and other entities
-    // related to this app installation
-    const currentState = await this.props.sdk.app.getCurrentState();
+    // Generate a new target state with the App assigned to the selected
+    // content types
+    const targetState = await this.createTargetState();
 
     return {
-      // Parameters to be persisted as the app configuration.
-      parameters: this.state.parameters,
-      // In case you don't want to submit any update to app
-      // locations, you can just pass the currentState as is
-      targetState: currentState,
+      targetState,
     };
   };
 
+  createTargetState = async () => {
+    const appId = this.props.sdk.ids.app;
+
+    const currentState = await this.props.sdk.app.getCurrentState();
+
+    // return {
+    //   // Transformation of an environment performed in the
+    //   // installation process.
+    //   targetState: {
+    //     EditorInterface: {
+    //       // Always pass in the `currentState` to avoid
+    //       // removing any previous location configuration.
+    //       ...currentState?.EditorInterface,
+    //       'category': {
+    //           editors: {
+    //             // Places the entry at the second position
+    //             // (position = 1) in the editors list for the
+    //             // content type with ID `my-content-type-id`.
+    //             widgetNamespace: "app",
+    //             widgetId: appId,
+    //             position: 4             
+    //           }
+    //       }
+    //     }
+    //   },
+    // };
+    
+    const EditorInterface = this.state.data.reduce(
+      (editorInterface: any, { active, contentType }) => {
+        if (active) {
+          console.log(contentType.sys.id)   
+          editorInterface[contentType.sys.id] = {
+            editors: [
+              {
+                widgetNamespace: "app",
+                widgetId: appId,
+                position: 4
+              },
+            ],
+          };
+        }
+        console.log(editorInterface);
+        return editorInterface;
+      },
+      currentState?.EditorInterface || {}
+    );
+
+    return { ...currentState, EditorInterface };
+  };
+
+  async componentDidMount() {
+    const data = await this.getContentTypesUsingEditor();
+    this.setState({ data }, () => {
+      this.props.sdk.app.setReady();
+    });
+  }
+
+  async getContentTypesUsingEditor() {
+    const { space, ids } = this.props.sdk;
+
+    const editorInterfaces = await space.getEditorInterfaces();
+    const appId = ids.app;
+
+    const appIncludedInEditors = (appId: any, editorInterface: any) => {
+      if (editorInterface.editor) {
+        return appId === editorInterface.editor.widgetId;
+      } else if (editorInterface.editors) {
+        return editorInterface.editors.some(({ widgetId }: any) => widgetId === appId);
+      } else {
+        return false;
+      }
+    };
+
+    return Promise.all(
+      editorInterfaces.items.map(async (ei: any) => {
+        const contentTypeId = ei.sys?.contentType?.sys?.id;
+        const contentType = await space.getContentType(contentTypeId);
+
+        //return { contentType, active: appIncludedInEditors(appId, ei) };
+        return { contentType, active: true };
+      })
+    );
+  }
+
+  toggleElement(index: number, current: boolean) {
+    this.setState(state => {
+      state.data[index].active = !current;
+      return state;
+    });
+  }
+
   render() {
     return (
-      <Workbench className={css({ margin: '80px' })}>
-        <Form>
-          <Heading>App Config</Heading>
-          <Paragraph>Welcome to your contentful app. This is your config page.</Paragraph>
-        </Form>
-      </Workbench>
+      <>
+        <Heading>Select which Content Types will include the JSON Viewer</Heading>
+        <List>
+          {this.state.data.map(({ active, contentType }, index) => (
+            <ListItem key={contentType.sys.id}>
+              <Switch
+                id={contentType.sys.id}
+                labelText={contentType.name}
+                isChecked={active}
+                onToggle={() => this.toggleElement(index, active)}
+              />
+            </ListItem>
+          ))}
+        </List>
+      </>
     );
   }
 }
--- src/components/EntryEditor.tsx
+++ src/components/EntryEditor.tsx
@@ -1,13 +1,38 @@
-import React from 'react';
-import { Paragraph } from '@contentful/forma-36-react-components';
+import React, {useState, useEffect} from 'react';
+import { Paragraph, SkeletonDisplayText } from '@contentful/forma-36-react-components';
 import { EditorExtensionSDK } from '@contentful/app-sdk';
+import { setSyntheticLeadingComments } from 'typescript';
+// import the react-json-view component
+import ReactJson from 'react-json-view';
 
 interface EditorProps {
   sdk: EditorExtensionSDK;
 }
 
 const Entry = (props: EditorProps) => {
-  return <Paragraph>Hello Entry Editor Component</Paragraph>;
+  const [json, setJson] = useState(JSON.stringify({}));
+  const [sys, setSys] = useState({});
+  
+  const { sdk } = props;
+  
+  //get the entryId
+  const entryId = sdk.entry.getSys().id;
+  
+  //refresh the entry when there's a change, to get latest changes
+  useEffect(() => {
+    sdk.entry.onSysChanged((sys) => {
+      setSys(sys);
+    })
+  })
+  
+  // @ts-ignore
+  useEffect(async () => {
+    const data = await sdk.space.getEntry(entryId);
+    setJson(JSON.stringify(data, null, 2))
+  }, [sys]);
+
+  return (<ReactJson src={JSON.parse(json)} displayDataTypes={false} /> 
+  );
 };
 
 export default Entry;
