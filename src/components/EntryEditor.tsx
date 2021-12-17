bf284622ece896cb3b58c2ca2d086c314147dd2e
Luke Wertz <luke@lukewertz.com>
2021-12-17 14:04:19 -0500

    Add an admin toolbar with Nested Item Depth option
    Refs #7: This PR adds an admin/option toolbar to the JSON Viewer.
    
    It also adds the ability to choose how many nested levels of includes you want.

--- src/components/EntryEditor.tsx
+++ src/components/EntryEditor.tsx
@@ -2,6 +2,8 @@ import React, {useState, useEffect} from 'react';
 import { EditorExtensionSDK } from '@contentful/app-sdk';
 // import the react-json-view component
 import ReactJson from 'react-json-view';
+import { Accordion, AccordionItem, Dropdown, DropdownList, DropdownListItem,
+  SectionHeading, Note, Paragraph, Button } from '@contentful/forma-36-react-components';
 
 interface EditorProps {
   sdk: EditorExtensionSDK;
@@ -10,27 +12,68 @@ interface EditorProps {
 const Entry = (props: EditorProps) => {
   const [json, setJson] = useState(JSON.stringify({}));
   const [sys, setSys] = useState({});
-  
-  const { sdk } = props;
-  
+  const [isOpen, setOpen] = useState(false);
+  const [depthState, SetDepthState] = useState(1);
+
+  const { sdk, cma } = props;
+
   //get the entryId
   const entryId = sdk.entry.getSys().id;
-  
+
   //refresh the entry when there's a change, to get latest changes
   useEffect(() => {
     sdk.entry.onSysChanged((sys) => {
       setSys(sys);
     })
   })
-  
+
+  // Handle Nested Include Depth
+  const setDepthHandler = (event: React.MouseEvent<Element, MouseEvent>, depth: React.SetStateAction<number>) => {
+    SetDepthState(depth);
+    setOpen(!isOpen); // close the select list
+  }
+
   // @ts-ignore
   useEffect(async () => {
-    const data = await sdk.space.getEntry(entryId);
+    const data = await cma.entry.references({
+      entryId: entryId,
+      include: depthState
+    });
     setJson(JSON.stringify(data, null, 2))
-  }, [sys]);
+  }, [sys, depthState]);
 
-  return (<ReactJson src={JSON.parse(json)} displayDataTypes={false} /> 
-  );
+
+  return (<>
+    <Accordion>
+      <AccordionItem title={<SectionHeading>JSON Viewer Options</SectionHeading>}>
+
+        <Dropdown
+          isOpen={isOpen}
+          onClose={() => setOpen(false)}
+          toggleElement={
+            <Button size="small" buttonType="muted" indicateDropdown onClick={() => setOpen(!isOpen)}>
+              Include Depth: {depthState}
+            </Button>
+          }
+        >
+          <DropdownList>
+            <DropdownListItem onClick={(event) => setDepthHandler(event, 1)}>1 Nested Levels</DropdownListItem>
+            <DropdownListItem onClick={(event) => setDepthHandler(event, 2)}>2 Nested Levels</DropdownListItem>
+            <DropdownListItem onClick={(event) => setDepthHandler(event, 3)}>3 Nested Levels</DropdownListItem>
+            <DropdownListItem onClick={(event) => setDepthHandler(event, 4)}>4 Nested Levels</DropdownListItem>
+            <DropdownListItem onClick={(event) => setDepthHandler(event, 5)}>5 Nested Levels</DropdownListItem>
+            <DropdownListItem onClick={(event) => setDepthHandler(event, 6)}>6 Nested Levels</DropdownListItem>
+            <DropdownListItem onClick={(event) => setDepthHandler(event, 7)}>7 Nested Levels</DropdownListItem>
+            <DropdownListItem onClick={(event) => setDepthHandler(event, 8)}>8 Nested Levels</DropdownListItem>
+            <DropdownListItem onClick={(event) => setDepthHandler(event, 9)}>9 Nested Levels</DropdownListItem>
+            <DropdownListItem onClick={(event) => setDepthHandler(event, 10)}>10 Nested Levels</DropdownListItem>
+          </DropdownList>
+        </Dropdown>
+
+      </AccordionItem>
+    </Accordion>
+    <ReactJson src={JSON.parse(json)} displayDataTypes={false} />
+  </>);
 };
 
 export default Entry;
