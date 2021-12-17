df8bb41e2f0231f7bec08d046d785ad68b4f8e1d
Luke Wertz <luke@lukewertz.com>
2021-12-17 11:32:44 -0500

    Refs #3: Adds includes.

--- src/components/EntryEditor.tsx
+++ src/components/EntryEditor.tsx
@@ -1,35 +1,39 @@
 import React, {useState, useEffect} from 'react';
 import { EditorExtensionSDK } from '@contentful/app-sdk';
+import { PlainClientAPI } from 'contentful-management';
 // import the react-json-view component
 import ReactJson from 'react-json-view';
 
 interface EditorProps {
   sdk: EditorExtensionSDK;
+  cma: PlainClientAPI;
 }
 
 const Entry = (props: EditorProps) => {
   const [json, setJson] = useState(JSON.stringify({}));
   const [sys, setSys] = useState({});
-  
-  const { sdk } = props;
-  
+
+  const { sdk, cma } = props;
+
   //get the entryId
   const entryId = sdk.entry.getSys().id;
-  
+
   //refresh the entry when there's a change, to get latest changes
   useEffect(() => {
     sdk.entry.onSysChanged((sys) => {
       setSys(sys);
     })
   })
-  
+
   // @ts-ignore
   useEffect(async () => {
-    const data = await sdk.space.getEntry(entryId);
+    // const data = await sdk.space.getEntry(entryId);
+    const data = await cma.entry.references({entryId});
     setJson(JSON.stringify(data, null, 2))
   }, [sys]);
 
-  return (<ReactJson src={JSON.parse(json)} displayDataTypes={false} /> 
+  return (
+    <ReactJson src={JSON.parse(json)} displayDataTypes={false} />
   );
 };
 
